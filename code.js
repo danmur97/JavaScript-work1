671b15fe00e074da9997067dabe6cc6f19a86533
danmur97 <danmur97@outlook.com>
2018-08-22 11:22:00 -0500

    random colors and dinamic creation

---
+++ code.js
@@ -0,0 +1,10 @@
+var box_ref= document.getElementsByClassName("box");
+var new_box;
+for(i = 0;i<20;i++){
+    new_box =  document.createElement("div");
+    new_box.className = "box";
+    new_box.style.backgroundColor = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")"; 
+    // new_box.style.backgroundColor = "rgb("+toString(Math.round(Math.random()*255))+","+ toString(Math.round(Math.random()*255))+","+ toString(Math.round(Math.random()*255))+")"; 
+    document.getElementById("main").appendChild(new_box);
+    
+}
