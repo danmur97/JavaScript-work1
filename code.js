9a694861cbf8358f6742f1f1317448ec80da3c3a
danmur97 <danmur97@outlook.com>
2018-08-22 11:44:10 -0500

    alternative class set

--- code.js
+++ code.js
@@ -2,9 +2,8 @@ var box_ref= document.getElementsByClassName("box");
 var new_box;
 for(i = 0;i<20;i++){
     new_box =  document.createElement("div");
-    new_box.className = "box";
+    // new_box.className = "box";
     new_box.style.backgroundColor = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")"; 
-    // new_box.style.backgroundColor = "rgb("+toString(Math.round(Math.random()*255))+","+ toString(Math.round(Math.random()*255))+","+ toString(Math.round(Math.random()*255))+")"; 
+    new_box.classList.add("box");
     document.getElementById("main").appendChild(new_box);
-    
 }
