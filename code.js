a36160dca4926426823d8b48b3fbf5e48f22867d
luisaAlvarez <sadfghj>
2018-08-23 11:06:11 -0500

    timers included

--- code.js
+++ code.js
@@ -1,9 +1,25 @@
-var box_ref= document.getElementsByClassName("box");
-var new_box;
-for(i = 0;i<20;i++){
-    new_box =  document.createElement("div");
-    // new_box.className = "box";
-    new_box.style.backgroundColor = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")"; 
-    new_box.classList.add("box");
-    document.getElementById("main").appendChild(new_box);
+var box_list = [];
+createBoxes();
+timer = setInterval(boxes_setColor,500);
+timer2 = setInterval(timer_change,2000);
+// clearTimeout(timer);
+function createBoxes(){
+    var new_box;
+    for(i = 0;i<20;i++){
+        new_box =  document.createElement("div");
+        // new_box.className = "box";
+        new_box.style.backgroundColor = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")"; 
+        new_box.classList.add("box");
+        document.getElementById("main").appendChild(new_box);
+        box_list.push(new_box);
+    }
+}
+function boxes_setColor(){
+    for(i = 0;i<20;i++){
+        box_list[i].style.backgroundColor = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")"; 
+    }
+}
+function timer_change(){
+    clearTimeout(timer);
+    timer = setInterval(boxes_setColor, Math.round(Math.random()*1000)+500);
 }
